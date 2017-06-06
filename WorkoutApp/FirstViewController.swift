108278a6ccc1b02c0e4a0a0891e309fcf038eeb5
rbendary94 <rana.bendary94@gmail.com>
2017-06-06 18:20:23 +0200

    Initializing project with Login and Sign up Views

---
+++ WorkoutApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WorkoutApp
+//
+//  Created by Rana on 6/6/17.
+//  Copyright © 2017 Rana. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
