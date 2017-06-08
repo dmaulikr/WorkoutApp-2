55ccdc52bde1e282250a5cb5e5598d4e20499c4c
nesreenM <nesreen.mouti@gmail.com>
2017-06-08 23:01:26 +0200

    Programs view and on shake barcode generation

--- WorkoutApp/FirstViewController.swift
+++ WorkoutApp/FirstViewController.swift
@@ -20,6 +20,15 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+ 
+    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        print("Device was shaken!")
+       
+        let storyBoard : UIStoryboard = UIStoryboard(name:"Programs", bundle:nil)
+        // test change to bar code genrator 
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "BarcodeViewController") as! BarcodeViewController
+        self.present(nextViewController, animated:true, completion:nil)
+    }
 
 }
 
